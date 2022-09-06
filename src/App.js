9aeaa6236499a7db79bb6e13224366ff1360005e
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-06 12:48:03 -0500

    set up react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      < Header />
+      <div className="container">
+        < Balance />
+      </div>
+    </div>
+  );
+}
+
+export default App;
