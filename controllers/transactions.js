074e874633c270bd3dc4844f46304050aacd142b
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-07 15:45:45 -0500

    post route created

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,28 +3,32 @@ const Transaction = require('../models/Transaction');
 // description  Get all transactions
 // route    GET /api/v1/transactions
 // access   Public
-exports.getTransactions = async (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     try {
-        const transactions = await Transaction.find();
-
-        return res.status(200).json({
-            success: true,
-            count: transactions.length,
-            data: transactions
-        });
+      const { text, amount } = req.body;
+  
+      const transaction = await Transaction.create(req.body);
+    
+      return res.status(201).json({
+        success: true,
+        data: transaction
+      }); 
     } catch (err) {
-        return res.sendStatus(500).json({
-            success: false,
-            error: 'Server Error'
+      if(err.name === 'ValidationError') {
+        const messages = Object.values(err.errors).map(val => val.message);
+  
+        return res.status(400).json({
+          success: false,
+          error: messages
         });
+      } else {
+        return res.status(500).json({
+          success: false,
+          error: 'Server Error'
+        });
+      }
     }
-}
-// description  Add a transaction
-// route    POST /api/v1/transactions
-// access   Public
-exports.addTransaction = async (req, res, next) =>
-    res.send('POST transaction');
-
+  }
 // description  Delete a transaction
 // route    DELETE /api/v1/transactions
 // access   Public
