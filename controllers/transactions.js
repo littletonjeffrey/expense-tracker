c5363e0833dd5a069a43f69f4a4b63dc7705b823
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-07 07:58:52 -0500

    post and delete routes created, connected to server and controller, tested in insomnia

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,4 +2,16 @@
 // route    GET /api/v1/transactions
 // access   Public
 exports.getTransactions = (req, res, next) =>
-    res.send('GET transcriptions');
+    res.send('GET transaction');
+
+// description  Add a transaction
+// route    POST /api/v1/transactions
+// access   Public
+exports.addTransaction = (req, res, next) =>
+    res.send('POST transaction');
+
+// description  Delete a transaction
+// route    DELETE /api/v1/transactions
+// access   Public
+exports.deleteTransaction = (req, res, next) =>
+    res.send('DELETE transaction');
