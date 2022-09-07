12e84bb995134c2451a5c950b7e9cb9ef7233538
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-07 14:19:11 -0500

    made transaction model and tested get call with insomnia

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,17 +1,32 @@
+const Transaction = require('../models/Transaction');
+
 // description  Get all transactions
 // route    GET /api/v1/transactions
 // access   Public
-exports.getTransactions = (req, res, next) =>
-    res.send('GET transaction');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
 
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.sendStatus(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+}
 // description  Add a transaction
 // route    POST /api/v1/transactions
 // access   Public
-exports.addTransaction = (req, res, next) =>
+exports.addTransaction = async (req, res, next) =>
     res.send('POST transaction');
 
 // description  Delete a transaction
 // route    DELETE /api/v1/transactions
 // access   Public
-exports.deleteTransaction = (req, res, next) =>
+exports.deleteTransaction = async (req, res, next) =>
     res.send('DELETE transaction');
