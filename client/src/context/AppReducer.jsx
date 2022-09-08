3363fbe49838543791b95d42d3e9b4ae82020970
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-07 21:55:04 -0500

    updated id to _id

--- client/src/context/AppReducer.jsx
+++ client/src/context/AppReducer.jsx
@@ -1,18 +1,17 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
-switch(action.type) {
-    case 'GET_TRANSACTION':
+    switch(action.type) {
+      case 'GET_TRANSACTIONS':
         return {
-            ...state,
-            loading: false,
-            transactions: action.payload
+          ...state,
+          loading: false,
+          transactions: action.payload
         }
-    
     case 'DELETE_TRANSACTION':
         return {
             ...state,
-            transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            transactions: state.transactions.filter(transaction => transaction._id !== action.payload)
         }
 
     case 'ADD_TRANSACTION':
--- client/src/context/GlobalState.jsx
+++ client/src/context/GlobalState.jsx
@@ -1,36 +1,37 @@
-import { createContext, useReducer } from 'react';
-import axios from 'axios';
+import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
-//Initial State
+import axios from 'axios';
+
+// Initial state
 const initialState = {
-    transactions: [],
-    error: null,
-    loading: true
+  transactions: [],
+  error: null,
+  loading: true
 }
 
 // Create context
 export const GlobalContext = createContext(initialState);
 
-//Provider component
-export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-//Actions
-async function getTransactions(){
+  // Actions
+  async function getTransactions() {
     try {
-const res = await axios.get('/api/v1/transactions/');
+      const res = await axios.get('/api/v1/transactions');
 
-    dispatch({
+      dispatch({
         type: 'GET_TRANSACTIONS',
         payload: res.data.data
-    })
+      });
     } catch (err) {
-        dispatch({
-            type: 'TRANSACTION_ERROR',
-            payload: err.response.data.error
-        })
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error
+      });
     }
-}
+  }
 
 function deleteTransaction(id) {
     dispatch({
